9bfc736baedd6cc010376d516f3480886d3bb2e0
Ryan Hardiman <r.j.hardiman@outlook.com>
2024-12-02 10:28:42 +0000

    More succinct

--- 2024/day 2.R
+++ 2024/day 2.R
@@ -3,17 +3,15 @@ library(tidyverse)
 data<-get_aoc(2)|>str_extract_all("\\d+")|>lapply(as.numeric)
 
 check <- function(x){
-  all(head(x,-1)-tail(x,-1)<0 & head(x,-1)-tail(x,-1)> -4 )|
-    all(head(x,-1)-tail(x,-1)>0 & head(x,-1)-tail(x,-1)< 4 )
+  all((head(x,-1)-tail(x,-1)) %in% -3:-1)|
+    all((head(x,-1)-tail(x,-1))%in% 1:3)
 }
 
 #Part1 390
 map(data, check)|>unlist()|>sum()
 
 remove_1 <- function(x){
-  out<-list()
-  for(i in 1:length(x)) out<-c(out,list(x[-i]))
-  return(out)
+  map(1:length(x),  ~x[-.x]|>check())|>unlist()|>any()
 }
 #part2 439
-map(data,~.x|>remove_1()|>map(\(y)check(y))|>unlist()|>any())|>unlist()|>sum()
+map(data,remove_1)|>unlist()|>sum()
