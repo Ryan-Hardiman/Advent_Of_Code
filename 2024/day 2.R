324797e6172c7d5544b4e87968d5deb54e3fe07f
Ryan Hardiman <r.j.hardiman@outlook.com>
2024-12-02 05:36:59 +0000

    Day 2

---
+++ 2024/day 2.R
@@ -0,0 +1,21 @@
+source("get_aoc.R")
+library(tidyverse)
+data<-get_aoc(2)
+data<- data|>str_extract_all("\\d+")|>lapply(as.numeric)
+
+tst<-clipr::read_clip()|>str_extract_all("\\d+")|>lapply(as.numeric)
+check <- function(x){
+  all(head(x,-1)-tail(x,-1)<0 & head(x,-1)-tail(x,-1)> -4 )|
+    all(head(x,-1)-tail(x,-1)>0 & head(x,-1)-tail(x,-1)< 4 )
+}
+
+#Part1
+map(data, check)|>unlist()|>sum()
+
+remove_1 <- function(x){
+  out<-list()
+  for(i in 1:length(x)) out<-c(out,list(x[-i]))
+  return(out)
+}
+#part2
+map(data,~.x|>remove_1()|>map(\(y)check(y))|>unlist()|>any())|>unlist()|>sum()
