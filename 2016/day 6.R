2587a9e58a70992e7549c7f9ac23aafef3646e12
Ryan Hardiman <79624586+Ryan-Hardiman@users.noreply.github.com>
2025-06-23 13:58:44 +0100

    Update day 6.R
    Shorter Solution

--- 2016/day 6.R
+++ 2016/day 6.R
@@ -1,44 +1,24 @@
 library(tidyverse)
 data <- get_aoc(2016,4)
 
-parse_data <- function(x){
+parse_data <- function(x,fn){
   x|>str_extract_all(".")|>
-    lapply(map, \(x) str_count(x,letters))
-}
-
-
-get_counts <- function(x){
-  reduce(x, \(acc,nxt){
-         map2(acc,nxt, `+`)})
-}
-
-
-return_most_common <- function(x){
-  map_chr(x, \(x) letters[which(x == max(x))])|>
+    lapply(map, \(x) str_count(x,letters))|>
+    reduce(\(acc,nxt) map2(acc,nxt,`+`))|>
+    map_chr(~letters[which(.x == do.call(fn,list(.x)))])|>
     glue::glue_collapse()
 }
-
-return_least_common <- function(x){
-  map_chr(x, \(x) letters[which(x == min(x))])|>
-    glue::glue_collapse()
-}
-
 
 part_1 <- function(x){
-  x|>parse_data()|>
-    get_counts()|>
-    return_most_common()
+  x|>parse_data("max")
 }
 
 part_2 <- function(x){
-  x|>parse_data()|>
-    get_counts()|>
-    return_least_common()
+  x|>parse_data("min")
 }
-
-
 
 #Answers:
 part_1(data) #wkbvmikb
 part_2(data) #evakwaga
+
 
