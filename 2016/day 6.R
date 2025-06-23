4f42a41757aee79b698f3beffdacd2185f0c673f
Ryan Hardiman <r.j.hardiman@outlook.com>
2025-06-23 18:44:02 +0100

    Further golfed.

--- 2016/day 6.R
+++ 2016/day 6.R
@@ -1,20 +1,20 @@
 library(tidyverse)
-data <- get_aoc(2016,4)
+data <- get_aoc(6,2016)
 
 parse_data <- function(x,fn){
   x|>str_extract_all(".")|>
     lapply(map, \(x) str_count(x,letters))|>
     reduce(\(acc,nxt) map2(acc,nxt,`+`))|>
-    map_chr(~letters[which(.x == do.call(fn,list(.x)))])|>
+    map_chr(~letters[which(.x == fn(.x))])|>
     glue::glue_collapse()
 }
 
 part_1 <- function(x){
-  x|>parse_data("max")
+  x|>parse_data(max)
 }
 
 part_2 <- function(x){
-  x|>parse_data("min")
+  x|>parse_data(min)
 }
 
 #Answers:
