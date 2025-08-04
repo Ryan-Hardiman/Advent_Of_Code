560d0599f8f5a2b1043f3aec348901775b859750
Ryan Hardiman <79624586+Ryan-Hardiman@users.noreply.github.com>
2025-08-04 12:46:46 +0100

    Update utils.R
    Minor change to Pad day <10 with 0. 
    Affects visual ordering of files

--- utils.R
+++ utils.R
@@ -1,8 +1,8 @@
 new_day <- function(day = lubridate::today()|>lubridate::day(),
                     year=lubridate::today()|>lubridate::year()){
-  out_path <- glue::glue("{year}/day {day}.R")
+  out_path <- glue::glue("{year}/day {stringr::str_pad(day, width = 2, pad = '0')}.R")
   if(file.exists(here::here(out_path))){stop("ALREADY EXISTS")}
-  temp<-whisker::whisker.render(readr::read_file("template.R"), data = list(day = day, year = year))
+  temp<-whisker::whisker.render(readr::read_file("template.R"), data = list(day = stringr::str_pad(day, width = 2, pad = "0"), year = year))
   
   readr::write_file(temp, file = out_path)
   source(out_path)
