5c8b935e66206b7e47c7c1ab1926cfa0bb9e054e
Ryan Hardiman <r.j.hardiman@outlook.com>
2025-12-15 20:14:15 +0000

    day 07 cleaned

--- 2025/day 07.R
+++ 2025/day 07.R
@@ -1,62 +1,49 @@
-source("get_aoc.R") #Pull input from website, prints input + stores it 
-library(tidyverse) #Almost always required
-data<- get_aoc(07,2025)
+source("get_aoc.R")
+library(tidyverse)
 
-#Make each line a Boolean vec. S required for initial state.
-parse_data <- function(x){
-  x%>%str_split("")%>%map(`%in%`,c("S","^"))
+data <- get_aoc(7, 2025)
+
+# Parse input into logical vectors:
+# TRUE for S or ^, FALSE otherwise
+parse_data <- function(x) {
+  str_split(x, "") |> map(`%in%`, c("S", "^"))
 }
 
-
-do_next <- function(acc,nxt){
+step_day7 <- function(acc, row) {
   
-  #Part 2 introduces timelines, we store the number of timelines in the name of
-  # acc[[1]] This avoids needing to do possible 2^n work..
-  timelines <- names(acc[[1]])%>%as.numeric()
+  timelines <- acc$timelines
+  has_beam  <- timelines > 0
   
-  to_split <-acc[[1]] & nxt 
-  to_fall_through <- acc[[1]] & !nxt
-  split <- lag(to_split,default = F) | lead(to_split,default = F)
-  #Part 1 Score is current score + number of splits.
-  acc[[2]] <- acc[[2]] + sum(to_split) 
-  #Propagate any new split beam or ones that weren't split.
-  acc[[1]] <- split|to_fall_through 
+  to_split <- has_beam & row
+  to_fall  <- has_beam & !row
   
+  # Part 1: count activated splitters
+  acc$splits <- acc$splits + sum(to_split)
   
-  #Main body of logic for part 2, temporarily collect the values that are split.
-  tmp <- timelines * to_split
-  #Add them to the next row, for left / right, and remove them from the next row in place.
-  timelines <- timelines + lag(tmp,default = 0) + lead(tmp,default = 0) - timelines*to_split
-  #naming must come last
-  acc[[1]] <- set_names(acc[[1]], timelines)
+  # Part 2: propagate timelines
+  split_mass <- timelines * to_split
+  
+  acc$timelines <-
+    timelines * to_fall +
+    lag(split_mass,  default = 0) +
+    lead(split_mass, default = 0)
   
   acc
 }
 
-
-
-
-
-part_1 <-function(x){
-lines <- parse_data(x)
-reduce(tail(lines,-1),
-       do_next,
-       .init = list(
-         set_names(lines[[1]],1*lines[[1]]) #Bit wasteful here.. 
-         ,0))[[2]]
+solve_day7 <- function(x) {
+  rows <- parse_data(x)
+  
+  init <- list(
+    timelines = as.numeric(rows[[1]]),  # S = 1, rest = 0
+    splits    = 0
+  )
+  
+  reduce(rows[-1], step_day7, .init = init)
 }
 
+out <- solve_day7(data)
 
-part_2 <-function(x){
-  lines <- parse_data(x)
-  out<-reduce(tail(lines,-1),
-         do_next,
-         .init = list(
-           set_names(lines[[1]],1*lines[[1]]),0))
-  #Collect timelines and sum them.
-  out[[1]]%>%names()%>%as.numeric()%>%sum()
-}
-
-#Answers:
-part_1(data) # 1642
-part_2(data) # 47274292756692
+# Answers
+out$splits          # Part 1: 1642
+sum(out$timelines)  # Part 2: 47274292756692
